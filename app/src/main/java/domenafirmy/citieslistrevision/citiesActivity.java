09f6ab7ddc72603cd6d4cd3e7487fe3974ba65ab
RafalJaworski <admin@lux-price.pl>
2015-10-26 10:21:30 +0100

    get recyclerView widget

--- app/src/main/java/domenafirmy/citieslistrevision/citiesActivity.java
+++ app/src/main/java/domenafirmy/citieslistrevision/citiesActivity.java
@@ -2,12 +2,17 @@ package domenafirmy.citieslistrevision;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class citiesActivity extends AppCompatActivity {
+
+    private RecyclerView rv ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities);
+
+        rv = (RecyclerView) findViewById(R.id.list);
     }
 }
