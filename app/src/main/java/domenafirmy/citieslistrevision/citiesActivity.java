c542c750db03df300ad802ddbd2ac8033a7b58b1
RafalJaworski <admin@lux-price.pl>
2015-10-26 10:23:49 +0100

    List of Cities example

--- app/src/main/java/domenafirmy/citieslistrevision/citiesActivity.java
+++ app/src/main/java/domenafirmy/citieslistrevision/citiesActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class citiesActivity extends AppCompatActivity {
 
     private RecyclerView rv ;
@@ -14,5 +17,15 @@ public class citiesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cities);
 
         rv = (RecyclerView) findViewById(R.id.list);
+
+        List<String> cities =  new ArrayList<>();
+        cities.add("Aberdeen");
+        cities.add("Edinburg");
+        cities.add("Glasgow");
+        cities.add("Invernes");
+        cities.add("Chester");
+        cities.add("London");
+        cities.add("Liverpool");
+        cities.add("Manchester");
     }
 }
